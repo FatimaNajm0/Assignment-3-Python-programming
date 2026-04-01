759d70503b97c334461eac7255d517b7020bb54f
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-01 23:48:38 +0300

    Create Q8.py

---
+++ Q8.py
@@ -0,0 +1,7 @@
+# Define a function named 'greet_morning'
+def greet_morning():
+    # Print the required message when the function is executed
+    print("Good Morning!")
+
+# Call the function to test it
+greet_morning()
