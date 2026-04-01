a39993b866164d275fffb1751cda4d4eafabddb5
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-01 23:40:47 +0300

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,9 @@
+# Define the number of rows for the pattern
+rows = 5
+
+# Loop from 1 to 5 (inclusive)
+# range(1, 6) means it starts at 1 and stops before 6
+for i in range(1, rows + 1):
+    # In Python, multiplying a string by a number repeats that string
+    # For example, '*' * 3 will result in '***'
+    print('*' * i)
