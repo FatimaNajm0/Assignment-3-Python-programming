cd850722d6f75c5d04949586b06b5f18a94464b3
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-01 23:50:04 +0300

    Create Q10.py

---
+++ Q10.py
@@ -0,0 +1,13 @@
+# Define a function that takes two numbers as parameters (num1 and num2)
+def print_sum(num1, num2):
+    # Calculate the sum of the two numbers and store it in a variable
+    total = num1 + num2
+    
+    # Print the final result
+    print("The sum of the two numbers is:", total)
+
+# Call the function and pass two numbers to it (for example, 5 and 7)
+print_sum(5, 7)
+
+# You can call it again with different numbers
+print_sum(10, 25)
